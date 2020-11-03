a2c6e74015dd47e817dd58c35b0d3869ec73b006
Fini Cafferata <FINI@MBP-de-Josefina.fibertel.com.ar>
2020-11-03 16:34:57 -0300

    S22 a S27

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -10,7 +10,29 @@ server.get('/', (req, res, next) => {
 		.catch(next);
 });
 
-server.get('/products/:id', (req, res, next) => {
+// Task S22: Crear ruta que devuelva los productos de X categoría
+server.get('/categoria/:category', (req, res, next) => {
+    Category.findOne(req.params.category, {
+        include: {model: Product}
+    })
+    .then(products => {res.status(200).send(products)})
+    .catch(next);
+});
+
+// Task S23: Crear ruta que retorne productos según el keyword de búsqueda
+// GET /search?query={valor}
+server.get('/search', (req, res, next) => {
+    Product.findAll({
+        where: {
+            name: {
+                [Sequelize.Op.iLike]: req.query.value,
+            }
+        }
+    })
+})
+
+// Task S24: Crear ruta de producto individual, pasado un ID que retorne un producto con sus detalles
+server.get('/:id', (req, res, next) => {
     Product.findOne({
         where: {
             productId: req.params.id
@@ -22,12 +44,34 @@ server.get('/products/:id', (req, res, next) => {
         } else {
             res.send(product)
         };
-	})
-	.catch(next);
+    })
+    .catch(next);
 });
 
-server.put('/products/:id', (req, res, next) => {
-    Product.findByPk(req.params.id)
+// Task S25: Crear ruta para crear/agregar Producto
+// POST /products
+server.post('/', (req, res, next) => {
+    const { ngoId, name, description, price, categoryId, image, stock } = req.body;
+    Product.create({
+        ngoId: ngoId,
+        name: name,
+        description: description, 
+        price: price, 
+        categoryId: categoryId, 
+        image: image, 
+        stock: stock,
+    })                
+    .then(cat => {
+        res.status(201).send(cat);
+    })
+    .catch(next);
+})
+
+// Task S26 : Crear ruta para Modificar Producto
+server.put('/:id', (req, res, next) => {
+    Product.update(req.body, {
+        productId: req.params.id
+    })
     .then(prod => {
         if(prod) res.status(200).send(prod);
         if(!prod) res.status(400).send({error: 'No se encontró ese ID de producto'})
@@ -35,11 +79,20 @@ server.put('/products/:id', (req, res, next) => {
     .catch(next);
 })
 
-server.get('/products/categoria/:category', (req, res, next) => {
-    Category.findByPk(req.params.category, {
-        include: {model: Product}
+// Task S27: Crear Ruta para eliminar Producto
+server.delete('/products/:id', (req, res, next) => {
+    Product.findOne({
+        where: {
+            productId: req.params.id 
+        }
     })
-    .then(products => {res.status(200).send(products)})
+    .then((prod) => {
+        if(!prod) res.status(400).send({error: 'No se encontró ese ID de producto'})
+        if(prod) {
+            prod.destroy();
+            res.sendStatus(200)
+        }
+    })
     .catch(next);
 });
 
