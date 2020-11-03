5196e3753c701aac3f292127d9740cd784344a7a
sebaTruisi <sebatruisi@gmail.com>
2020-11-03 18:36:40 -0300

    Inicio de tareaS5

--- api/src/db.js
+++ api/src/db.js
@@ -35,10 +35,13 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Product } = sequelize.models;
+const { Product, Category } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
+
+Product.belongsToMany(Category, { through: prodCat });
+Category.belongsToMany(Product, { through: prodCat });
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
