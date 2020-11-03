535137a85b2c5e7cfb36ea296e0c8d94222c1338
datatom7 <costanzo.tomas@gmail.com>
2020-11-03 19:13:09 -0300

    Resolución Parcial (v2)

--- api/src/routes/categories.js
+++ api/src/routes/categories.js
@@ -1,6 +1,18 @@
 const server = require('express').Router();
 const { Category } = require('../db.js');
 
+// S17: Crear ruta para agregar o sacar categorías de un producto
+
+server.post('/products/:productId/category/:categoryId', (req, res, next) => {
+    Product.findByPk(req.params.productId, {
+        include: {model: Category}         ///// Revisar con Fini.
+    })
+    .then(products => {res.status(200).send(products)})
+    .catch(next);
+});
+
+
+// S18: Crear ruta para crear/agregar categoría
 
 server.post('/product/category', (req, res, next) => {
     Category.findOrCreate({
@@ -15,13 +27,7 @@ server.post('/product/category', (req, res, next) => {
     .catch(next);
 });
 
-server.put('/product/category/:id', (req, res, next) => {
-    Category.findByPk(req.params.id) //
-    .then(cat => {
-        res.status(200).send(cat);
-    })
-    .catch(next);
-});
+// S19: Crear ruta para eliminar categoría
 
 server.delete('/products/category/:category', (req, res, next) => {
 	let category = req.params.category;
@@ -38,3 +44,14 @@ server.delete('/products/category/:category', (req, res, next) => {
         }    /// ¿Qué mando?
     })
 });
+
+// S20: Crear ruta para modificar categoría
+
+server.put('/product/category/:id', (req, res, next) => {
+    Category.findByPk(req.params.id) //
+    .then(cat => {
+        res.status(200).send(cat);
+    })
+    .catch(next);
+});
+
