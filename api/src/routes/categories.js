51ee73ed9f7d5dd9d69e2cafe8717caa34f2beed
datatom7 <costanzo.tomas@gmail.com>
2020-11-03 10:10:49 -0300

    Back in order!

--- api/src/routes/categories.js
+++ api/src/routes/categories.js
@@ -2,15 +2,39 @@ const server = require('express').Router();
 const { Category } = require('../db.js');
 
 
-server.delete('/products/category/:categoryId', (req, res, next) => {
-	let categoryId = req.params.categoryId;
+server.post('/product/category', (req, res, next) => {
+    Category.findOrCreate({
+        where: {
+            name: req.body.name,
+            description: req.body.description
+        }
+    })                
+    .then(cat => {
+            res.status(201).send(cat);
+        })
+    .catch(next);
+});
+
+server.put('/product/category/:id', (req, res, next) => {
+    Category.findByPk(req.params.id) //
+    .then(cat => {
+        res.status(200).send(cat);
+    })
+    .catch(next);
+});
+
+server.delete('/products/category/:category', (req, res, next) => {
+	let category = req.params.category;
 	Product.findOne({
         where: {
-            categoryId: categoryId 
+            name: category 
         }
     })
     .then((category) => {
-        category.destroy();
-        res.send(category)
+        if(!category) res.status(400).send({error: 'No se encontró ese ID de producto'})
+        if(category) {
+            category.destroy();
+            res.sendStatus(200)
+        }    /// ¿Qué mando?
     })
 });
