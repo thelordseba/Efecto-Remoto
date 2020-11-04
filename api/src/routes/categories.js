a8cffa889660ef2084d8bee5a32049641afd5fb7
Tomás Costanzo <43776192+DataTom7@users.noreply.github.com>
2020-11-04 13:49:52 -0300

    Update categories.js

--- api/src/routes/categories.js
+++ api/src/routes/categories.js
@@ -3,14 +3,6 @@ const { Category } = require('../db.js');
 
 // GET todas las categorías
 
-server.post('/products/:productId/category/:categoryId', (req, res, next) => {
-    Product.findByPk(req.params.productId, {
-        include: {model: Category}         ///// Revisar con Fini. ¿Cómo modifico la tabla intermedia? catProd
-    })
-    .then(products => {res.status(200).send(products)})
-    .catch(next);
-});
-
 server.get('/', (req, res, next) => {
     Category.findAll()
     .then(categories => res.send(categories))
