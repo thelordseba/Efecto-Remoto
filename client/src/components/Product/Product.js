52674ad96f7c40f181aeda36324c977f5390c18b
laraortiz95 <61565270+laraortiz95@users.noreply.github.com>
2020-11-04 15:56:54 -0300

    Producto modificado/finish

--- client/src/components/Product/Product.js
+++ client/src/components/Product/Product.js
@@ -7,9 +7,12 @@ function Product(props) {
 
   props = {
     titulo: 'Zapatilla',
-    descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    descripcion: 'Esta compra será para ayudar a la ONG Fundación Potrero. El Potrero se funda a partir de la motivación de un grupo de amigos con el fin de fomentar la igualdad de oportunidades de niños y jóvenes alrededor del país.',
     precio: '$1.400',
-    cantidad: 2,
+    cantidad: 'Cantidad: 1',
+    stock: 'Hasta agotar stock de 100 pares de zapatillas.',
+    stars: 3,
+    link: 'https://www.elpotrero.org/',
     image: 'https://topperarg.vteximg.com.br/arquivos/ids/211016-1200-1200/025433.jpg?v=636979578311500000'
   }
 
@@ -25,9 +28,12 @@ function Product(props) {
           <div className="title">{props.precio}</div>
           <div className="divider"/>
           <div className="description">{props.descripcion}</div>
+          <div className="link"> <span>Ver más en :</span> <a href ={props.link}>{props.link}</a> </div>
           <div className="divider"/>
           <div className="cantidad">{props.cantidad}</div>
-          <Stars/>
+          <div className="stock">{props.stock}</div>
+          <div className="review">Review</div>
+           <Stars disabledClick={true} stars={props.stars}/> 
           <div className="button" onClick={handleOnClick}>Add to Cart</div>
         </div>
       <div>
