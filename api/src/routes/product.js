9ac9e09b99c29b1eddf9c015139238a95dc93587
Josefina Cafferata Leloir <67561181+finicafferata@users.noreply.github.com>
2020-11-04 14:54:03 -0300

    04112020 1453

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -1,6 +1,6 @@
 const server = require('express').Router();
 const { Product } = require('../db.js');
-const { Category } = require('../db.js')
+const { Category } = require('../db.js');
 const { Sequelize } = require('sequelize');
 
 // Task S17: Crear ruta para agregar o sacar categorías de un producto
@@ -38,30 +38,39 @@ server.get('/', (req, res, next) => {
 });
 
 // Task S22: Crear ruta que devuelva los productos de X categoría
-server.get('/categories/:categoryId', (req, res, next) => {
-    Category.findAll(req.params.categoryId, {
-        include: {model: Product}
+server.get('/categories/:catName', (req, res, next) => {
+    const catName = req.params.catName;
+    Product.findAll({
+        where: { 
+            [Sequelize.Op.or]: [
+                { category: { [Sequelize.Op.substring]: catName} }
+            ]
+        },
     })
-    .then(products => {res.status(200).send(products)})
+    .then(products => {
+        res.send(products);
+    })
     .catch(next);
 });
 
 // Task S23: Crear ruta que retorne productos según el keyword de búsqueda
 // GET /search?query={valor}
 server.get('/search', (req, res, next) => {
+    const valor = req.query.query;
+
     Product.findAll({
-        where: {
-            [Sequelize.Op.iLike]: req.query.name,
-        }
+      where: {
+        [Sequelize.Op.or]: [
+          { name: { [Sequelize.Op.substring]: valor } },
+          { description: { [Sequelize.Op.substring]: valor } },
+        //   { category: { [Sequelize.Op.substring]: valor} },
+        ],
+      },
     })
-    .then(product => {
-        if(!product) { 
-            res.status(400).send({error: 'No se identificó ese producto'});
-        } else {
-            res.send(product)
-        }
+    .then((products) => {
+        res.send(products);
     })
-    .catch(next);
+    .catch((err) => res.send(err));
 })
 
 // Task S24: Crear ruta de producto individual, pasado un ID que retorne un producto con sus detalles
@@ -80,15 +89,15 @@ server.get('/:id', (req, res, next) => {
 // Task S25: Crear ruta para crear/agregar Producto
 // POST /products
 server.post('/', (req, res, next) => {
-    const { ngoId, name, description, price, categoryId, image, stock } = req.body;
+    const { ngoId, name, description, price, category, image, stock } = req.body;
     Product.create(
         {
         ngoId: ngoId,
         name: name,
         description: description, 
         price: price, 
-        categoryId: categoryId, 
-        image: image, 
+        category: category, 
+        img: image, 
         stock: stock,
         }
     )            
