c9779b8acf849af72303bb0dfaa1b3ee8310684c
RocioPerez <69337501+RocioPerez@users.noreply.github.com>
2020-11-06 20:58:43 -0300

    home

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -5,8 +5,19 @@ import './Home.css';
 function Home(){
     return (
     <div>
-        <img className="Imagen" src={img} alt= "Imagen no encontrada" /> 
+        
+        <div>
+            <img className="Imagen" src={img} alt= "Imagen no encontrada" /> 
+        </div>
+        <div>
+            <button>Usuario</button>
+            <button>Administrador</button>
+        </div>
+
     </div>
+    
+    
+    
     )
 }
 export default Home;
