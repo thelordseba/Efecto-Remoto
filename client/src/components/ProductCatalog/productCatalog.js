8fde8e5d5e231f1bf0c42129f0b09ceb89c1bfc8
Josefina Cafferata Leloir <67561181+finicafferata@users.noreply.github.com>
2020-11-07 21:26:22 -0300

    Correcciones para la demo

--- client/src/components/ProductCatalog/productCatalog.js
+++ client/src/components/ProductCatalog/productCatalog.js
@@ -1,27 +1,49 @@
 import React, {useEffect, useState} from 'react';
 import ProductCard from '../ProductCard/ProductCard.js';
-import FilterCategories from './filterCategories.js';
 import axios from 'axios'
 import { useHistory } from "react-router-dom"
 
 function ProductCatalog ({admin}){
   const [products, setProducts] = useState([])
+  const [categories, setCategories] = useState([])
+  // const [prodByCat, setProdByCat] = useState([])
   const history = useHistory();
 
   const handleOnClickAddProduct = () => {
     history.push(`/product/add`)
   }
 
+  // const handleOnClickFilterByCategory = () => {
+
+  // }
+
   useEffect( () => {(async () => {
       const products = await axios.get(`http://localhost:3001/products`)
       setProducts(products.data)  
-    })()}, [])
+  })()}, [])
   
   console.log(products)
+
+  useEffect( () => {(async () => {
+      const categories = await axios.get(`http://localhost:3001/categories/`)
+      setCategories(categories.data)
+  })()}, [])
+
+  // useEffect(() => {(async () => {
+  //     const prodByCat = await axios.get(`http://localhost:3001/products/categories/:categoryId`)
+  //     setProdByCat(prodByCat.data)
+  // })()}, [])
   
   return (
       <div>
-          <FilterCategories />
+          <label>Seleccioná una categoría: </label>
+          <select>
+            {/* <option value="" disabled selected>Categorías</option> */}
+            <option value="allCategories">Todas las categorías</option>
+            {categories.map((categories) => 
+            <option value={categories}>{categories.name}</option> 
+            )}
+          </select>
           {admin ? <div className="button" onClick={handleOnClickAddProduct}>Agregar producto</div> : null} 
           {products.map((product) => 
           <ProductCard
