50120976f6a5edb53d9cbe4f118eb4f19065b548
Tomás Costanzo <43776192+DataTom7@users.noreply.github.com>
2020-11-08 08:14:06 -0300

    08112020 0814

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -3,9 +3,6 @@ import ProductCatalog from "../components/ProductCatalog/productCatalog";
 import ProductDetail from "../components/ProductDetail/ProductDetail.js";
 import Menu from "../components/Menu/Menu.js";
 import Success from "../components/Success";
-
-//import Home from "../components/Home";
-
 import CreateUpdateProduct from "../components/Product_CRUD/CreateUpdateProduct"
 import FormCategorias from "../components/FormCategory/FormCategory";
 import Home from "../components/Home";
