43360793e3345d2b6f0d59f49ec824c18839f960
Josefina Cafferata Leloir <67561181+finicafferata@users.noreply.github.com>
2020-11-08 09:39:48 -0300

    Formato del CRUD

--- client/src/components/Product_CRUD/CreateUpdateProduct.js
+++ client/src/components/Product_CRUD/CreateUpdateProduct.js
@@ -69,20 +69,21 @@ function CreateUpdateProduct({id}){
 
     return (
         <div>
-        <h1 className="tituloForm">{id ? 'Actualizar' : 'Crear'} Producto</h1>
-        <form className="">
-        <input className="input1" onChange={handleInputChange} value={product ? product.ngoId : ""} name="ngoId" required type="text" placeholder="Ingrese Producto ONG" /><br /><br />
-        <input className="input2" onChange={handleInputChange} value={product ? product.name : ""} name="name" required type="text" placeholder="Ingrese el título del producto" /><br /><br />
-        <input className="input3"onChange={handleInputChange} value={product ? product.description : ""} name="description" required type="text" placeholder="Ingrese la descripción del producto" /><br /><br />
-        <input className="input4" onChange={handleInputChange} value={product ? product.categoryId : ""} name="categoria" required type="text" placeholder="Ingrese categoría del producto" /><br /><br />
-        <input className="input5" onChange={handleInputChange} value={product ? product.price : ""} name="price" required type="text" placeholder="Ingrese el precio del producto" /><br /><br />
-        <input className="input6" onChange={handleInputChange} value={product ? product.stock : ""} name="stock" required type="text" placeholder="Ingresar stock del producto" /><br /><br />
-        <input className="input7" onChange={handleInputChange} value={product ? product.img : ""} name="img" required type="text" placeholder="Ingrese la imagen del producto" /><br /><br />
-        <img src={src} alt={"Imagen no encontrada"}/>
-        {/* {src !== "" ? <div> <input onChange={onImageChange} name="img" required type="file" placeholder="Upload Product Image" /><br /><br /> </div>: null} */}
-        <button className="button" onClick={handleOnClick}>{id ? 'ACTUALIZAR' : 'CREAR'}</button>
-        {/* <button onClick={handleOnClick}>{id ? 'DELETE' : 'NODELETE'}</button> */}
-        </form>
+            <h1 className="tituloForm">{id ? 'Actualizar' : 'Crear'} Producto</h1>
+            <br /><br />
+            <form className="">
+                <input className="input1" onChange={handleInputChange} value={product ? product.ngoId : ""} name="ngoId" required type="text" placeholder="ONG" /><br /><br />
+                <input className="input2" onChange={handleInputChange} value={product ? product.name : ""} name="name" required type="text" placeholder="Título del producto" /><br /><br />
+                <input className="input3" onChange={handleInputChange} value={product ? product.description : ""} name="description" required type="text" placeholder="Descripción del producto" /><br /><br />
+                <input className="input4" onChange={handleInputChange} value={product ? product.categoryId : ""} name="categoria" required type="text" placeholder="Categoría del producto" /><br /><br />
+                <input className="input5" onChange={handleInputChange} value={product ? product.price : ""} name="price" required type="number" placeholder="Precio del producto ($)" /><br /><br />
+                <input className="input6" onChange={handleInputChange} value={product ? product.stock : ""} name="stock" required type="number" placeholder="Stock del producto" /><br /><br />
+                <input className="input7" onChange={handleInputChange} value={product ? product.img : ""} name="img" required type="text" placeholder="URL de la imagen del producto" /><br /><br />
+                <img src={src} alt={"Imagen no encontrada"}/>
+                {/* {src !== "" ? <div> <input onChange={onImageChange} name="img" required type="file" placeholder="Upload Product Image" /><br /><br /> </div>: null} */}
+                <button className="button" onClick={handleOnClick}>{id ? 'ACTUALIZAR' : 'CREAR'}</button>
+                {/* <button onClick={handleOnClick}>{id ? 'DELETE' : 'NODELETE'}</button> */}
+            </form>
         </div>
     );
 }
