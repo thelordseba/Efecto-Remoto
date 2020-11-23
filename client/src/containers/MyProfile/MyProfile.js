46508019b6f691890939453993fb748315354d42
OrtizLara <ortizlaraeliana@gmail.com>
2020-11-23 10:40:28 -0300

    my profile

--- client/src/containers/MyProfile/MyProfile.js
+++ client/src/containers/MyProfile/MyProfile.js
@@ -1,15 +1,10 @@
 import React from 'react';
+import OrderTable from '../OrderTable/OrderTable'
 
 
 export default function MyProfile({id}) { 
   return(
-      <nav className="nav">
-          <ul className="nav__ul">
-              <li className="nav__li"><a href={`/profile/${id}/data`}>Mis Datos</a></li>
-              <a href={`/profile/${id}/orders`}>Mis Ordenes</a>
-              <li className="nav__li"><a href="#">Cerrar Sesión</a></li>
-              <li className="nav__li"><a href="#">Ayuda</a></li>
-          </ul>
-      </nav>
-  )
+    <OrderTable userId={id}/>
+    )
+  
 };
