8190eba635c29b1b9d7d0d4fc0b40017bcd7314b
Tomás Costanzo <43776192+DataTom7@users.noreply.github.com>
2020-11-25 08:51:03 -0300

    25112020 0851

--- client/src/containers/Dashboard/Dashboard.js
+++ client/src/containers/Dashboard/Dashboard.js
@@ -1,11 +1,39 @@
-import React from "react";
-import Linear from "graphs/Linear.js";
+import React, { useState, useEffect } from "react";
+import OrderStatusPie from "graphs/OrderStatusPie/OrderStatusPie";
+import DailyOrdersLinear from "graphs/DailyOrdersLinear/DailyOrdersLinear.js";
+import DailyRevenueLinear from "graphs/DailyRevenueLinear/DailyRevenueLinear.js";
+import QAIndex from "graphs/QAIndex/QAIndex.js";
+import axios from "axios";
+import "containers/Dashboard/Dashboard.css";
 
 const Dashboard = () => {
+  const [ratings, setRatings] = useState();
+
+  useEffect(() => {
+    (async () => {
+      const allOrders = await axios.get(`http://localhost:3001/orders/`);
+      const ratings = await allOrders.data.map((order) => order.rating);
+      setRatings((ratings.reduce((acc, el) => acc + el) / ratings.length) * 10);
+    })();
+  }, []);
+
   return (
     <>
-      <h1>ESTO ES EL DASHBOARD</h1>;
-      <Linear />
+      <h1>ESTO ES DASHBOARD</h1>;
+      <div className="graph-grid">
+        <div className="graph-grid-0">
+          <QAIndex rating={ratings} />
+        </div>
+        <div className="graph-grid-1">
+          <DailyRevenueLinear />
+        </div>
+        <div className="graph-grid-2">
+          <OrderStatusPie />
+        </div>
+        <div className="graph-grid-3">
+          <DailyOrdersLinear />
+        </div>
+      </div>
     </>
   );
 };
