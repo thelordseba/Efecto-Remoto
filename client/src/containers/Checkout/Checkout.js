4dcf3c701ba1bc0347e3f96d540de62d917531e2
Tomás Costanzo <43776192+DataTom7@users.noreply.github.com>
2020-11-25 18:58:43 -0300

    25112020 1858

--- client/src/containers/Checkout/Checkout.js
+++ client/src/containers/Checkout/Checkout.js
@@ -9,67 +9,68 @@ import axios from "axios";
 const Checkout = () => {
   const history = useHistory();
   const order = useSelector((state) => state.order);
-  const currentUser = useSelector(state => state.currentUser);
-  const [data, setData] = useState({});  
+  const currentUser = useSelector((state) => state.currentUser);
+  const [data, setData] = useState({});
 
   const dispatch = useDispatch();
 
   const getTotal = () =>
     order.products?.reduce(
-      (acc, el) => acc + el.orderLine.price * el.orderLine.quantity, 0
+      (acc, el) => acc + el.orderLine.price * el.orderLine.quantity,
+      0
     );
 
-  const updateUser = async (id, userData)=>{   
-    try{
+  const updateUser = async (id, userData) => {
+    try {
       await axios.put(`http://localhost:3001/users/${id}`, userData);
-    }catch(error){  
-        alert("No se pudo actulizar datos de usuario.")
-    } 
-  }  
+    } catch (error) {
+      alert("No se pudo actulizar datos de usuario.");
+    }
+  };
 
-  useEffect(
-    () => {
-      if(currentUser?.id){
-        dispatch(getUserById(currentUser.id));
-        dispatch(getOrderByUserId(currentUser.id));        
-      } 
-    },
-    [dispatch, currentUser.id]
-  );
+  useEffect(() => {
+    if (currentUser?.id) {
+      dispatch(getUserById(currentUser.id));
+      dispatch(getOrderByUserId(currentUser.id));
+    }
+  }, [dispatch, currentUser.id]);
 
   const handleBack = () => {
     history.push(`/carrito`);
   };
-  
+
   const toPayment = async (id) => {
-    const { data } = await axios.post(`${process.env.REACT_APP_API}/payment/${id}/toPayment`);
+    const { data } = await axios.post(
+      `${process.env.REACT_APP_API}/payment/${id}/toPayment`
+    );
     window.location = data.redirect;
   };
 
-  const handlePayment = () => {       
-    if(!currentUser.email || !currentUser.location){
+  const handlePayment = () => {
+    if (!currentUser.email || !currentUser.location) {
       updateUser(currentUser.id, data);
-    } 
-    toPayment(order.id)
+    }
+    toPayment(order.id);
   };
 
-  const handleOnChange = (e)=>{
+  const handleOnChange = (e) => {
     setData({
-        ...data,
-        [e.target.name]: e.target.value
-    })
-}
+      ...data,
+      [e.target.name]: e.target.value,
+    });
+  };
 
   return (
-    <>      
+    <>
       <div className="checkout-container">
         <div className="container-summary">
           <div className="title-container-summary">Productos</div>
-          <div className="divider-summary" />         
-          {order.products?.map(( product ) => (                               //order.products && order.products.map()
-             <OrderLine name={product.name} orderLine={product.orderLine} /> )
-            ) 
-          }
+          <div className="divider-summary" />
+          {order.products?.map((
+            product //order.products && order.products.map()
+          ) => (
+            <OrderLine name={product.name} orderLine={product.orderLine} />
+          ))}
           <div className="divider-summary" />
           <div className="total">Total: ${getTotal()} </div>
         </div>
@@ -78,19 +79,36 @@ const Checkout = () => {
         <div className="container-summary">
           <div className="title-container-summary">Orden de compra:</div>
           <div className="divider-summary" />
-          <div>Nombre completo: {" " + currentUser.firstName + " " + currentUser.lastName}</div>
-          <div>Email: {!currentUser.email ? (<input></input>) : (currentUser.email)}</div>
+          <div>
+            Nombre completo:{" "}
+            {" " + currentUser.firstName + " " + currentUser.lastName}
+          </div>
+          <div>
+            Email: {!currentUser.email ? <input></input> : currentUser.email}
+          </div>
           <div>
             Dirección de facturación:{" "}
-            {!currentUser.location ? 
-            (<div>
-              <div><label>Calle: {" "}</label><input name= "address" onChange={handleOnChange}></input></div>
-              <div><label>Número: {" "}</label><input name= "number" onChange={handleOnChange}></input></div>
-              <div><label>Provincia: {" "}</label><input name= "city" onChange={handleOnChange}></input></div>
-              <div><label>Código postal: {" "}</label><input name= "postalCode" onChange={handleOnChange}></input></div>              
-             </div>
-            ): (currentUser.location?.address +
-
+            {!currentUser.location ? (
+              <div>
+                <div>
+                  <label>Calle: </label>
+                  <input name="address" onChange={handleOnChange}></input>
+                </div>
+                <div>
+                  <label>Número: </label>
+                  <input name="number" onChange={handleOnChange}></input>
+                </div>
+                <div>
+                  <label>Provincia: </label>
+                  <input name="city" onChange={handleOnChange}></input>
+                </div>
+                <div>
+                  <label>Código postal: </label>
+                  <input name="postalCode" onChange={handleOnChange}></input>
+                </div>
+              </div>
+            ) : (
+              currentUser.location?.address +
               " " +
               currentUser.location?.number +
               " - " +
@@ -103,8 +121,14 @@ const Checkout = () => {
         </div>
       </div>
       <div className="payment-button">
-        <div className="back2" onClick={handleBack}> Seguir comprando </div>
-        <div className="payment" onClick={handlePayment}> Proceder al pago </div>        
+        <div className="back2" onClick={handleBack}>
+          {" "}
+          Seguir comprando{" "}
+        </div>
+        <div className="payment" onClick={handlePayment}>
+          {" "}
+          Proceder al pago{" "}
+        </div>
       </div>
     </>
   );
