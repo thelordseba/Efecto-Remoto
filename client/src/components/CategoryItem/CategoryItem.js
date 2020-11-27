2edc2689d84f963b20bb9d260a89ca8b7ec1a835
Tomás Costanzo <43776192+DataTom7@users.noreply.github.com>
2020-11-27 13:56:04 -0300

    27112020 1355 Bug Fixing CategoryItem

--- client/src/components/CategoryItem/CategoryItem.js
+++ client/src/components/CategoryItem/CategoryItem.js
@@ -1,32 +1,34 @@
-import React from 'react'
+import React from "react";
 
-function CategoryItem({id, titulo, photo}){
-console.log("titulo" ,titulo)
-    return (
-        <div
-        className="cat-cnt-css"
-        style={{  position: 'relative',
-            display: 'grid',
-            'border-radius': '5px',
-            'margin-bottom': '8px',
-            'background-color': '#fff',
-            'box-shadow': '0 0 0.125rem #c3c3c3',
-            'transition-duration': '250ms',
-            'background-position': 'center center',
-	        'background-size': 'cover',
-	        backgroundRepeat: 'no-repeat',
-            overflow: 'hidden',
-            backgroundImage: `url(${photo})`,
-            height: '300px'}}
-      >
-        <div className="product-card-content">
-          <a href={`/products?category=${id}`}>
-            <div className="title-categor">{titulo}</div>
-          </a>
-        </div>
-        <div></div>
+function CategoryItem({ id, titulo, photo }) {
+  console.log("titulo", titulo);
+  return (
+    <div
+      className="cat-cnt-css"
+      style={{
+        position: "relative",
+        display: "grid",
+        borderRadius: "5px",
+        marginBottom: "8px",
+        backgroundColor: "#fff",
+        boxShadow: "0 0 0.125rem #c3c3c3",
+        transitionDuration: "250ms",
+        backgroundPosition: "center center",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        overflow: "hidden",
+        backgroundImage: `url(${photo})`,
+        height: "300px",
+      }}
+    >
+      <div className="product-card-content">
+        <a href={`/products?category=${id}`}>
+          <div className="title-categor">{titulo}</div>
+        </a>
       </div>
-  )
+      <div></div>
+    </div>
+  );
 }
-   
-    export default CategoryItem
+
+export default CategoryItem;
