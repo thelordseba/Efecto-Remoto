c2c9b00a20c54fd04712519ea6018c7716a4472e
“celeste.martins” <celeste.martins@sirena.app>
2020-11-28 20:41:01 -0300

    finish founders

--- client/src/containers/Home/Home.js
+++ client/src/containers/Home/Home.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Nosotros from "components/Nosotros/Nosotros.js";
 import ReviewPage from "containers/ReviewPage/ReviewPage.js";
-import CarrouselONGs from "components/Founders/CarrouselONGs.js";
+import CarrouselONGs from "components/CarrouselONGs/CarrouselONGs.js";
+import Founders from "components/Founders/Founders.jsx";
 
 function Home() {
   return (
     <div>
       <Nosotros />
       <ReviewPage />
+      <Founders/>
       <CarrouselONGs />
     </div>
   );
