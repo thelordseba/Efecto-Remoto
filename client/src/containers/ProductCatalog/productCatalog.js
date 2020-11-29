ac3dc3ec07be387cb2695d726a8fe62be2a39820
Josefina Cafferata Leloir <67561181+finicafferata@users.noreply.github.com>
2020-11-29 18:39:11 -0300

    fixPagination

--- client/src/containers/ProductCatalog/productCatalog.js
+++ client/src/containers/ProductCatalog/productCatalog.js
@@ -29,8 +29,8 @@ function ProductCatalog({ home, admin, latest }) {
 
   const dispatch = useDispatch();
   const { products, countProducts } = useSelector((state) => state);
-  const maxPages = useMemo(() => Math.ceil(countProducts / limit), [
-    countProducts,
+  const maxPages = useMemo(() => Math.ceil(products.length / limit), [
+    products.length,
   ]);
   const search = useSelector((state) => state.search);
 
