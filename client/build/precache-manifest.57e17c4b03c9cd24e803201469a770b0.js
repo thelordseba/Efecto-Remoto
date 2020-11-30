ff62a155b4f96244d004a4fec8c015fc7701d155
Josefina Cafferata Leloir <67561181+finicafferata@users.noreply.github.com>
2020-11-30 09:55:01 -0300

    sacamos build
    sacamos build en el archivo gitignore

---
+++ client/build/precache-manifest.57e17c4b03c9cd24e803201469a770b0.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "388b633cb16fff78e83bcb519f31ee19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8127a88c0acf43d7ee88",
+    "url": "/static/css/2.437d98bc.chunk.css"
+  },
+  {
+    "revision": "7a476f31ac3309975182",
+    "url": "/static/css/main.1cc6b8a1.chunk.css"
+  },
+  {
+    "revision": "8127a88c0acf43d7ee88",
+    "url": "/static/js/2.95d79dd2.chunk.js"
+  },
+  {
+    "revision": "e2a4044c3457a83c0e9218106019a618",
+    "url": "/static/js/2.95d79dd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a476f31ac3309975182",
+    "url": "/static/js/main.b15453a2.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "5ec1015594a657df655813384dd6e149",
+    "url": "/static/media/0.5ec10155.png"
+  },
+  {
+    "revision": "f3dbe3949b5401d784758061ba95e429",
+    "url": "/static/media/1.f3dbe394.png"
+  },
+  {
+    "revision": "5dcb40812b83a264a2790be247eabca4",
+    "url": "/static/media/2.5dcb4081.png"
+  },
+  {
+    "revision": "740e1ca90394fb66dcb141a3b7f181b9",
+    "url": "/static/media/3.740e1ca9.png"
+  },
+  {
+    "revision": "11c97e14e783e60d4059e766ca157226",
+    "url": "/static/media/4.11c97e14.png"
+  },
+  {
+    "revision": "d4dcc27e3516c573c01df2ed419f6c73",
+    "url": "/static/media/5.d4dcc27e.png"
+  },
+  {
+    "revision": "9b2baaf0bed8545740b93a115871a2f2",
+    "url": "/static/media/All.9b2baaf0.png"
+  },
+  {
+    "revision": "bfeade12487daf1d1fa0417706ee3249",
+    "url": "/static/media/Cancel.bfeade12.png"
+  },
+  {
+    "revision": "af97de466af38cb6247052d37242114f",
+    "url": "/static/media/Succes.af97de46.png"
+  },
+  {
+    "revision": "2a980289fd3b0d7598f42bfafc2e3463",
+    "url": "/static/media/cart.2a980289.svg"
+  },
+  {
+    "revision": "3403c522df4fc3c5a966b9db94c06676",
+    "url": "/static/media/emptyRating.3403c522.png"
+  },
+  {
+    "revision": "ffb63eb50c133dd9ea862b7be68ae963",
+    "url": "/static/media/fullRating.ffb63eb5.png"
+  },
+  {
+    "revision": "9fda108dc73e9031035e83f0b054325c",
+    "url": "/static/media/imag-nos.9fda108d.jpg"
+  },
+  {
+    "revision": "56b628a31fe7301bba89db540025eb93",
+    "url": "/static/media/persona1.56b628a3.jpg"
+  },
+  {
+    "revision": "8ebcae52020758bfb51550f6c30f285a",
+    "url": "/static/media/persona2.8ebcae52.jpg"
+  },
+  {
+    "revision": "0626a822e4d31aada48ef6c76a3d3161",
+    "url": "/static/media/persona3.0626a822.jpg"
+  },
+  {
+    "revision": "8a76d5fba24038d2c5b74b8cc9ba0975",
+    "url": "/static/media/persona4.8a76d5fb.jpg"
+  },
+  {
+    "revision": "665bfe94fca2119f0f569eb41139c24b",
+    "url": "/static/media/team.665bfe94.jpg"
+  },
+  {
+    "revision": "740028d44a1c1c3d31566dd7e2702d56",
+    "url": "/static/media/trash.740028d4.svg"
+  }
+]);
