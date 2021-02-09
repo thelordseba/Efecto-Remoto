7587751563bc66fda34ac8f619bdcca7a12aed87
sebaTruisi <sebatruisi@gmail.com>
2021-02-09 20:55:35 -0300

    Para user admin 2

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ const { conn } = require('./src/db.js');
 // Syncing all the models at once.
 
 //Para Api en Heroku
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(process.env.PORT, () => {
     console.log("%s listening at 3000"); // eslint-disable-line no-console
   });
