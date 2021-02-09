36c4fa06b0ddc949256d11ef2557023b8b079146
sebaTruisi <sebatruisi@gmail.com>
2021-02-08 21:35:37 -0300

    Ajustando configuracion para deploy

--- api/src/db.js
+++ api/src/db.js
@@ -5,11 +5,12 @@ const path = require("path");
 
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
+
 const sequelize =
   process.env.NODE_ENV === "production"
     ? new Sequelize({
         database: DB_NAME,
-        dialect: "mysql",
+        dialect: "postgres",
         host: DB_HOST,
         port: 5432,
         username: DB_USER,
@@ -30,18 +31,9 @@ const sequelize =
         ssl: true,
       })
     : new Sequelize(
-        `mysql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/development`,
         { logging: false, native: false }
       );
-
-
-// const sequelize = new Sequelize(
-//   `mysql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
-//   {
-//     logging: false, // set to console.log to see the raw SQL queries
-//     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-//   }
-// );
 
 
 
